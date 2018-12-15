4fa6c62b9523c3814d784c492868058ea6ec10d5
David Dinh <dmdinh22@gmail.com>
2018-12-15 00:57:36 -0800

    create controller for adding todo item and wired up IBAction and IBOutlets

---
+++ AddToDoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddToDoViewController.swift
+//  ToDoApp
+//
+//  Created by David D on 12/15/18.
+//  Copyright © 2018 David D. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+}
